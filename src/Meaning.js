1e51d6562865d99808acb991a36696de4fbf28d9
MissHalle <100787203+MissHalle@users.noreply.github.com>
2022-07-04 14:59:36 -0500

    fixed and showing synonyms

--- src/Meaning.js
+++ src/Meaning.js
@@ -14,8 +14,6 @@ export default function Meaning(props) {
               {definition.definition}
               <strong>Example:</strong>
               <em>{definition.example}</em>
-              <br />
-
               <Synonyms synonyms={definition.synonyms} />
             </p>
           </div>
